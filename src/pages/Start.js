c638c7f5f547d482f5f1b3eab6bc4b346509575b
Subash Balasubramanian <subash.balasubramanian@trelleborg.com>
2022-10-11 18:24:58 +0530

    Added the home page content

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -6,16 +6,17 @@ const Start = () => {
   return (
     <Layout id="start">
       <section className="flex flex-col justify-center items-center min-h-screen">
-        <h1 className="text-5xl font-bold">Intro to React</h1>
+        <h1 className="text-5xl font-bold">React to Dummies</h1>
         <p className="mt-5 w-1/2 text-center">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          We're going to talk about what is <b>web</b>? <br />
+          The journey which the web took to be at this stage. <br />
+          Challenges which it faces and the <b>solutions</b> which it took.
         </p>
-        <Link className="rounded-full px-3 py-1 border" to={'/speaker'}>
-          Let's begin
-        </Link>
+        <div className="mt-4 homeBtn">
+          <Link className="rounded-full px-3 py-1 border" to={'/speaker'}>
+            Let us begin
+          </Link>
+        </div>
       </section>
     </Layout>
   );
