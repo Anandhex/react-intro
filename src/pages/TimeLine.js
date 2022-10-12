03a760f884910c718e44ea60294bacff1c24dd69
Anand Patil <anand.patil@trelleborg.com>
2022-10-12 19:58:21 +0530

    minor changes

--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from '../components/Layout';
-
+import SmallCard from '../components/SmallCard';
 const TimeLine = () => {
   return (
     <>
       <Layout id={'timeLine'}>
-        <section className="flex items-center justify-center flex-col min-h-screen  px-5">
+        <section className="flex items-center justify-center flex-col min-h-screen  px-12">
           <h3 className="text-2xl font-bold mb-5">TimeLine of Web</h3>
+          <div className="flex flex-wrap gap-3">
+            <SmallCard title="" />
+          </div>
         </section>
       </Layout>
     </>
