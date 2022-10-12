65f19d9e24068187e31d255cb17b2e90b3504208
Anand Patil <anand.patil@trelleborg.com>
2022-10-12 23:00:12 +0530

    added timeline

---
+++ src/components/TimeLineCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const TimeLineCard = ({
+  reverse = false,
+  year = '',
+  img = '',
+  content = '',
+  title = '',
+}) => {
+  return (
+    <div className={`flex items-center ${reverse ? 'flex-row-reverse' : ''} `}>
+      <h4 className="text-3xl ">{year}</h4>
+      <div className="h-[2px] w-52 bg-black"></div>
+      <div className="w-[40ch] rounded-lg p-3 small-card bg-white">
+        <h4 className="text-2xl font-bold flex items-center">
+          <img className="inline-block mr-3" width={40} src={img} />
+          <span className="inline-block">{title}</span>
+        </h4>
+        <hr className="border-b-2 my-2 border-t-0 border-[#F94E34]" />
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeLineCard;
--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import SmallCard from '../components/SmallCard';
+import TimeLineCard from '../components/TimeLineCard';
+
 const TimeLine = () => {
   return (
     <>
-      <Layout id={'timeLine'}>
-        <section className="flex items-center justify-center flex-col min-h-screen  px-12">
-          <h3 className="text-2xl font-bold mb-5">TimeLine of Web</h3>
-          <div className="flex flex-wrap gap-3">
-            <SmallCard title="" />
-          </div>
-        </section>
-      </Layout>
+      <Layout id={'timeLine'} background="bg-slate-200"></Layout>
     </>
   );
 };
