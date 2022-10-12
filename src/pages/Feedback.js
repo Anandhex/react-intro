48abcb67db627f8238b93d9fbb380acf8ada77be
Anand Patil <anand.patil@trelleborg.com>
2022-10-13 00:10:30 +0530

    minor changes

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -6,7 +6,7 @@ const Feedback = () => {
     <Layout id={'feedback'} background="bg-slate-200">
       <section className="grid place-content-center min-h-screen">
         <h3 className="text-4xl font-bold mb-5 text-center">
-          Any Feedback for US?
+          Any Feedback for <em>US</em>?
         </h3>
         <div className="flex gap-6">
           <div className="grid place-content-center">
