880ffcf3f9c79b00e80d8cc2840815b007e49733
Subash Balasubramanian <subash.balasubramanian@trelleborg.com>
2022-10-12 15:14:27 +0530

    Added the web content

--- src/pages/WhatIsWeb.js
+++ src/pages/WhatIsWeb.js
@@ -12,7 +12,7 @@ const WhatIsWeb = () => {
           imgUrl={online}
           header="What is Web?"
           desc={
-            'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages'
+            '<b>The Web</b> —  is the common name for the World Wide Web, a subset of the Internet consisting of the pages that can be accessed by a Web browser. The Web is not the same as the Internet. The Web is one of many applications built on top of the Internet.'
           }
         />
       </section>
@@ -23,7 +23,7 @@ const WhatIsWeb = () => {
           reverse={true}
           header="What is Document?"
           desc={
-            'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages'
+            'A document is a form of information that might be useful to a user or set of users. This information can be in digital and nondigital forms.Web pages are formatted in a language called Hypertext Markup Language (HTML). It this language that allows users to click through pages on the Web via links. The Web uses HTTP protocol to transmit data and share information. Browsers such as Internet Explorer, Google Chrome or Mozilla Firefox are used to access Web documents, or Web pages, which are connected via links.'
           }
         />
       </section>
